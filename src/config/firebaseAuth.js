8c101cb0740689dc3e8cf173e4693f2daebc6044
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2026-06-18 21:20:06 +0530

    Update Firebase configuration details
    Reviving chatbox app for deployment

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -4,34 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 
 // MAIN CONFIG
 const firebaseConfig = {
-    apiKey: "AIzaSyDztVn3utSbq0bXicfdzvdG0V_CuRp3ifg",
-    authDomain: "fir-15-02-24.firebaseapp.com",
-    databaseURL: "https://fir-15-02-24-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fir-15-02-24",
-    storageBucket: "fir-15-02-24.appspot.com",
-    messagingSenderId: "205120726655",
-    appId: "1:205120726655:web:1731db9e17b41293a3814a"
+  apiKey: "AIzaSyBHO-Yb1_O1J4o-QkepWwfU-yAggl3bbrA",
+  authDomain: "ghostaudit-95b0d.firebaseapp.com",
+  projectId: "ghostaudit-95b0d",
+  storageBucket: "ghostaudit-95b0d.firebasestorage.app",
+  messagingSenderId: "223366964234",
+  appId: "1:223366964234:web:abe0b41f7339075b1f6bf3",
+  measurementId: "G-1WPYXVJHYY"
 };
-
-// SIDDHART FIREBASE
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCNHrWFGtFFmGo4GJD-FtXLuQWtoLWevLc",
-//     authDomain: "test1-4df68.firebaseapp.com",
-//     projectId: "test1-4df68",
-//     storageBucket: "test1-4df68.appspot.com",
-//     messagingSenderId: "815448162877",
-//     appId: "1:815448162877:web:32d12f565e8fe957f7e054",
-//     measurementId: "G-245EMTYFEK"
-//   };
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB1hqOmDerLJ0kDLpJptXZyUF4LVlbo4Q8",
-//     authDomain: "project-21-02-2024.firebaseapp.com",
-//     projectId: "project-21-02-2024",
-//     storageBucket: "project-21-02-2024.appspot.com",
-//     messagingSenderId: "387897356422",
-//     appId: "1:387897356422:web:fb0a32415e17bd6bc52107"
-// };
 
 const app = initializeApp(firebaseConfig);
 
